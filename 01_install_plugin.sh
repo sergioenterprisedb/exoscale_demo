d629be6d4a40359c40c97994ed7e43ed10011a81
Sergio Romera <sergio.romera@enterprisedb.com>
2023-01-05 15:06:57 +0100

    Version 1.0

---
+++ 01_install_plugin.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+. ./config.sh
+printf "${green}curl -sSfL
+  https://github.com/cloudnative-pg/cloudnative-pg/raw/main/hack/install-cnpg-plugin.sh |
+  sh -s -- -b /usr/local/bin${reset}\n"
+
+curl -sSfL \
+  https://github.com/cloudnative-pg/cloudnative-pg/raw/main/hack/install-cnpg-plugin.sh | \
+  sh -s -- -b /usr/local/bin
